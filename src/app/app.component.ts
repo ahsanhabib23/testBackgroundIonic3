1afe90e6635c0a5c45d6f96ab71beb179517641e
ahsanhabib23 <ahsan.habib@brainstation-23.com>
2019-07-25 21:22:38 +0600

    Testing the App Background Mood Issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {BackgroundMode} from "@ionic-native/background-mode";
+
+declare var cordova: any;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private backgroundMode: BackgroundMode) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      /*Checking whether the App is running or not*/
+      setInterval(()=> console.log("App is Running ================================="), 1000);
+
+      /* Setting the Foreground and Background Mood*/
+      backgroundMode.setDefaults({
+        title: "DiaperSens is running...",
+        text: 'Continuously monitoring sensors'
+      });
+      cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
+      this.backgroundMode.enable();
       splashScreen.hide();
     });
   }
