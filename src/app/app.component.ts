65677a711a344957b620701c96b6447033d1b267
ahsanhabib91 <habibibnakther@gmail.com>
2019-07-28 17:05:09 +0600

    add platform check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,28 @@ declare var cordova: any;
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+	rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private backgroundMode: BackgroundMode) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      /*Checking whether the App is running or not*/
-      setInterval(()=> console.log("App is Running ================================="), 1000);
-
-      /* Setting the Foreground and Background Mood*/
-      backgroundMode.setDefaults({
-        title: "DiaperSens is running...",
-        text: 'Continuously monitoring sensors'
-      });
-      cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
-      this.backgroundMode.enable();
-      splashScreen.hide();
-    });
-  }
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private backgroundMode: BackgroundMode) {
+	platform.ready().then(() => {
+		// Okay, so the platform is ready and our plugins are available.
+		// Here you can do any higher level native things you might need.
+		statusBar.styleDefault();
+		/*Checking whether the App is running or not*/
+		// setInterval(()=> console.log("App is Running ================================="), 1000);
+		
+		if(platform.is('cordova')) {
+			/* Setting the Foreground and Background Mood*/
+			backgroundMode.setDefaults({
+			title: "DiaperSens is running...",
+			text: 'Continuously monitoring sensors'
+			});
+			cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
+			this.backgroundMode.enable();
+		}
+		
+		splashScreen.hide();
+	});
+	}
 }
 
